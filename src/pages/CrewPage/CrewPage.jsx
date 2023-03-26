5e9cb3e402b29c527de623ac07dcf3441b71d77b
Whitney <designedbywh@gmail.com>
2023-03-26 14:21:22 -0400

    fix responsiveness on smaller screen devices and remove unnecessary CSS

--- src/pages/CrewPage/CrewPage.jsx
+++ src/pages/CrewPage/CrewPage.jsx
@@ -1,13 +1,21 @@
 import "./CrewPage.css";
 import { data } from "../../data/data";
 import CrewList from "../../components/Crew/CrewList";
+import { RiArrowRightSLine } from "react-icons/ri";
+import { Link } from "react-router-dom";
 
 const CrewPage = () => {
   return (
     <div className="crewpage main">
-      <h1>
-        <span className="bold">02</span> meet your crew
-      </h1>
+      <div className="flex-wrapper">
+        <h1>
+          <span className="bold">02</span> meet your crew
+        </h1>
+        <Link className="next" to={"/technology"}>
+          space launch 101 <RiArrowRightSLine />
+        </Link>
+      </div>
+
       <CrewList data={data.crew} />
     </div>
   );
--- src/pages/DestinationPage/DesPage.jsx
+++ src/pages/DestinationPage/DesPage.jsx
@@ -1,13 +1,20 @@
 import "./DesPage.css";
 import { data } from "../../data/data";
 import PlanetList from "../../components/Destination/PlanetList";
+import { RiArrowRightSLine } from "react-icons/ri";
+import { Link } from "react-router-dom";
 
 const DesPage = () => {
   return (
     <div className="despage main">
-      <h1>
-        <span className="bold">01</span> pick your destination
-      </h1>
+      <div className="flex-wrapper">
+        <h1>
+          <span className="bold">01</span> pick your destination
+        </h1>
+        <Link className="next" to={"/crew"}>
+          pick your crew <RiArrowRightSLine />
+        </Link>
+      </div>
       <PlanetList data={data.destinations} />
     </div>
   );
